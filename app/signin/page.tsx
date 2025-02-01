ebdb16c706416c5d5aa0e0900afbc6e4e850851b
thanagonp <thanagonp@kkumail.com>
2025-02-02 02:17:27 +0700

    sign-page

---
+++ app/signin/page.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import { useState } from "react";
+
+export default function SignInPage() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    return (
+        <div className="signin-container">
+            <div className="signin-box">
+                <h1 className="text-2xl font-bold">Sign In</h1>
+
+                <div>Username</div>
+                <input type="text" value={username} onChange={(e) => setUsername(e.target.value)}/>
+
+                <div>Password</div>
+                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+
+                <button>Sign In</button>
+            </div>
+        </div>
+    );
+}
